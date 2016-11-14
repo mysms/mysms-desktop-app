f240c4af28eeae1094589147db68afcc6ffb2790
Marius Tobler <tobler.marius@gmail.com>
2016-11-14 10:22:41 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (49 of 49 strings)

--- translation/mysms_de.ts
+++ translation/mysms_de.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
     <name>CommonTab</name>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="119"/>
+        <location filename="../usersettingsdialog.cpp" line="119"></location>
         <source>Run mysms on Windows startup</source>
         <translation>mysms beim Start von Windows ausführen</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="125"/>
+        <location filename="../usersettingsdialog.cpp" line="125"></location>
         <source>Show mysms in taskbar</source>
         <translation>mysms in Taskleiste anzeigen</translation>
     </message>
@@ -17,52 +17,52 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.cpp" line="145"/>
+        <location filename="../mainwindow.cpp" line="145"></location>
         <source>mysms</source>
         <translation>mysms</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="440"/>
+        <location filename="../mainwindow.cpp" line="440"></location>
         <source>Systray</source>
         <translation>Infobereich</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="441"/>
+        <location filename="../mainwindow.cpp" line="441"></location>
         <source>The program will keep running in the system tray. To terminate the program, choose &lt;b&gt;Quit&lt;/b&gt; in the context menu of the system tray entry.</source>
         <translation>mysms ist im Hintergrund aktiv. Um mysms zu beenden, öffne das mysms Menü im Infobereich und wähle &lt;b&gt;Beenden&lt;/b&gt;.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="540"/>
+        <location filename="../mainwindow.cpp" line="540"></location>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="543"/>
+        <location filename="../mainwindow.cpp" line="543"></location>
         <source>&amp;Refresh</source>
         <translation>&amp;Aktualisieren</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="546"/>
+        <location filename="../mainwindow.cpp" line="546"></location>
         <source>&amp;Settings</source>
         <translation>&amp;Einstellungen</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="549"/>
+        <location filename="../mainwindow.cpp" line="549"></location>
         <source>Mute for 30 minutes</source>
         <translation>Ausschalten für 30 Minuten</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="552"/>
+        <location filename="../mainwindow.cpp" line="552"></location>
         <source>Mute for 1 hour</source>
         <translation>Ausschalten für 1 Stunde</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="555"/>
+        <location filename="../mainwindow.cpp" line="555"></location>
         <source>Mute for 3 hour</source>
         <translation>Ausschalten für 3 Stunden</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="558"/>
+        <location filename="../mainwindow.cpp" line="558"></location>
         <source>Mute for 8 hour</source>
         <translation>Ausschalten für 8 Stunden</translation>
     </message>
@@ -83,12 +83,12 @@
         <translation type="vanished">Ausschalten für 8 Stunden</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="561"/>
+        <location filename="../mainwindow.cpp" line="561"></location>
         <source>Notifications disabled</source>
         <translation>Benachrichtigungen deaktiviert</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="575"/>
+        <location filename="../mainwindow.cpp" line="575"></location>
         <source>Notifications</source>
         <translation>Benachrichtigungen</translation>
     </message>
@@ -139,17 +139,17 @@
 <context>
     <name>NotificationPopup</name>
     <message>
-        <location filename="../notificationpopup.cpp" line="69"/>
+        <location filename="../notificationpopup.cpp" line="69"></location>
         <source>Close pop-up</source>
         <translation>Popup schließen</translation>
     </message>
     <message>
-        <location filename="../notificationpopup.cpp" line="77"/>
+        <location filename="../notificationpopup.cpp" line="77"></location>
         <source>Mark message as read</source>
         <translation>Nachricht als gelesen markieren</translation>
     </message>
     <message>
-        <location filename="../notificationpopup.cpp" line="85"/>
+        <location filename="../notificationpopup.cpp" line="85"></location>
         <source>Delete message</source>
         <translation>Nachricht löschen</translation>
     </message>
@@ -161,12 +161,12 @@
         <translation type="obsolete">Nachricht(en) empfangen</translation>
     </message>
     <message>
-        <location filename="../notificationsummarywidget.cpp" line="61"/>
+        <location filename="../notificationsummarywidget.cpp" line="61"></location>
         <source>Compose new message</source>
         <translation>Neue Nachricht erstellen</translation>
     </message>
     <message>
-        <location filename="../notificationsummarywidget.cpp" line="72"/>
+        <location filename="../notificationsummarywidget.cpp" line="72"></location>
         <source>Close pop-up</source>
         <translation>Popup schließen</translation>
     </message>
@@ -174,12 +174,12 @@
 <context>
     <name>NotificationTab</name>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="139"/>
+        <location filename="../usersettingsdialog.cpp" line="139"></location>
         <source>Show notifications</source>
         <translation>Benachrichtigungen anzeigen</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="140"/>
+        <location filename="../usersettingsdialog.cpp" line="140"></location>
         <source>Privacy Mode - message text is not displayed</source>
         <translation>Privacy Mode - Nachrichtentext wird nicht angezeigt</translation>
     </message>
@@ -214,39 +214,39 @@
 <context>
     <name>SoundTab</name>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="168"/>
+        <location filename="../usersettingsdialog.cpp" line="168"></location>
         <source>Play sound, if ...</source>
         <translation>Klingelton abspielen, wenn ...</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="170"/>
-        <location filename="../usersettingsdialog.cpp" line="245"/>
-        <location filename="../usersettingsdialog.cpp" line="262"/>
+        <location filename="../usersettingsdialog.cpp" line="170"></location>
+        <location filename="../usersettingsdialog.cpp" line="245"></location>
+        <location filename="../usersettingsdialog.cpp" line="262"></location>
         <source>Single message</source>
         <translation>Einzelnachricht</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="171"/>
+        <location filename="../usersettingsdialog.cpp" line="171"></location>
         <source>Group message</source>
         <translation>Gruppennachricht</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="186"/>
+        <location filename="../usersettingsdialog.cpp" line="186"></location>
         <source>Select sound to play</source>
         <translation>Wähle den gewünschten Klingelton</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="210"/>
+        <location filename="../usersettingsdialog.cpp" line="210"></location>
         <source>Play this sound</source>
         <translation>Diesen Klingelton abspielen</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="216"/>
+        <location filename="../usersettingsdialog.cpp" line="216"></location>
         <source>Play sound only for first unread message</source>
         <translation>Klingelton nur bei erster ungelesener Nachricht abspielen</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="217"/>
+        <location filename="../usersettingsdialog.cpp" line="217"></location>
         <source>Ring if application is open</source>
         <translation>Klingeln wenn App geöffnet ist</translation>
     </message>
@@ -281,22 +281,22 @@
 <context>
     <name>UpdateDialog</name>
     <message>
-        <location filename="../updatedialog.cpp" line="42"/>
+        <location filename="../updatedialog.cpp" line="42"></location>
         <source>There is a new version of mysms available</source>
         <translation>Es ist eine neue Version von mysms verfügbar</translation>
     </message>
     <message>
-        <location filename="../updatedialog.cpp" line="48"/>
+        <location filename="../updatedialog.cpp" line="48"></location>
         <source>Install</source>
         <translation>Installieren</translation>
     </message>
     <message>
-        <location filename="../updatedialog.cpp" line="49"/>
+        <location filename="../updatedialog.cpp" line="49"></location>
         <source>Continue</source>
         <translation>Weiter</translation>
     </message>
     <message>
-        <location filename="../updatedialog.cpp" line="68"/>
+        <location filename="../updatedialog.cpp" line="68"></location>
         <source>Update version</source>
         <translation>Version aktualisieren</translation>
     </message>
@@ -304,22 +304,22 @@
 <context>
     <name>UserSettingsDialog</name>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="42"/>
+        <location filename="../usersettingsdialog.cpp" line="42"></location>
         <source>Common Settings</source>
         <translation>Allgemeine Einstellungen</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="43"/>
+        <location filename="../usersettingsdialog.cpp" line="43"></location>
         <source>Notifications</source>
         <translation>Benachrichtigungen</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="44"/>
+        <location filename="../usersettingsdialog.cpp" line="44"></location>
         <source>Sounds</source>
         <translation>Klingeltöne</translation>
     </message>
     <message>
-        <location filename="../usersettingsdialog.cpp" line="58"/>
+        <location filename="../usersettingsdialog.cpp" line="58"></location>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
@@ -327,7 +327,7 @@
 <context>
     <name>WebView</name>
     <message>
-        <location filename="../webview.cpp" line="78"/>
+        <location filename="../webview.cpp" line="78"></location>
         <source>Save File</source>
         <translation>Datei speichern</translation>
     </message>
