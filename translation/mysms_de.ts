cd8eb5c34cd6c30c9e91de1fbdc50d19bac4217f
Scott Giesman <scottiegiesman@gmail.com>
2020-08-20 07:46:44 +0000

    Translated using Weblate (German)
    Currently translated at 97.9% (49 of 49 strings)

--- translation/mysms_de.ts
+++ translation/mysms_de.ts
@@ -348,7 +348,7 @@
     </message>
     <message>
         <source>Update version</source>
-        <translation type="unfinished">Version aktualisieren</translation>
+        <translation>Version aktualisieren</translation>
     </message>
     <message>
         <source>New version online</source>
